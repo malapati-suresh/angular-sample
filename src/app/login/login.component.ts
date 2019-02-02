4388effcd254e0b10961d07e03da697c3b7e8ff1
Suresh R <suresh.m@mtwlabs.com>
2019-02-02 15:00:35 +0530

    template forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormGroupName } from '@angular/forms';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,48 +12,72 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   public loginForm: FormGroup
+  public formLogin: FormGroup
 
-  public formLogin : FormGroup
-  
-  
+  public email: any = '';
+  public password: any = '';
+
+  public loginFormData: any;
+
+  selectOptions = 'first';
+
+  genders = ['Male', 'Famale']
+
+  public genderOptions: any = '';
+
 
   ngOnInit() {
 
+
     this.formLogin = new FormGroup({
-      email : new FormControl("",[Validators.required,Validators.email]),
-      password: new FormControl ("", Validators.required)
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", Validators.required)
     })
 
-    this.loginForm = new FormGroup ({
-      formData:new FormGroup({
-      email : new FormControl ("", [Validators.required,Validators.email]),
-      password : new FormControl ("", Validators.required)
+    this.loginForm = new FormGroup({
+      formData: new FormGroup({
+        email: new FormControl("", [Validators.required, Validators.email]),
+        password: new FormControl("", Validators.required)
       })
-      
+
     })
+
   }
 
 
+  formSubmit() {
+    console.log(this.formLogin.value, "Forms")
+    if (this.formLogin.valid) {
 
-formSubmit(){
-  console.log(this.formLogin.value,"Forms")
-  if(this.formLogin.valid){
-    
-  }else{
-    this.formLogin.get('email').markAsTouched();
-    this.formLogin.get('password').markAsTouched();
+    } else {
+      this.formLogin.get('email').markAsTouched();
+      this.formLogin.get('password').markAsTouched();
+    }
   }
-}
 
 
-submitForm(){
-  console.log(this.loginForm.value,"Loginnn")
-  if(this.loginForm.valid){
-      
-  }else{
-    this.loginForm.get('formData').get('email').markAsTouched();
-    this.loginForm.get('formData').get('password').markAsTouched();
+  submitForm() {
+    console.log(this.loginForm.value, "Loginnn")
+    if (this.loginForm.valid) {
+
+    } else {
+      this.loginForm.get('formData').get('email').markAsTouched();
+      this.loginForm.get('formData').get('password').markAsTouched();
+    }
   }
-}
+
+
+  onSubmit() {
+    this.loginFormData = {
+      email: this.email,
+      password: this.password,
+      selectOptions: this.selectOptions,
+      genderOptions: this.genderOptions,
+    }
+
+    console.log(this.loginFormData, "Template Driven")
+
+  }
+
 
 }
